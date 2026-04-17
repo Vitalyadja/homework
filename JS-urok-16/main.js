9a37efcd5910ea687400e7181609d5a8d827174f
Vitalyadja <m9tacss1337@mail.ru>
2026-04-17 19:44:48 +0300

    Add files via upload

---
+++ JS-urok-16/main.js
@@ -0,0 +1,53 @@
+//Задача 1
+
+const a = prompt("Введите число", 0)
+const b = a % 2;
+
+if (b == 0){
+    alert(`Число ${a} четное!`)
+} else {
+    alert(`Число ${a} нечетное!`)
+}
+
+//Задача 2
+
+// const age = 28;
+// const discount = age <= 18 ? 10 : age <= 65 ? 20 : age > 65 ? 30 : console.log("А иначе быть и не может ! У тебя скидка 30% !") ;
+// console.log(`Скидка ${discount}%`);
+
+//Задача 3
+
+// const userName = prompt("Введите имя", 0)
+// const password = prompt("Введите пароль", 0)
+
+// if ((userName === "admin" || userName === "user") && password === "123456"){
+//     alert("Доступ разрешен !")
+//     } else {
+//     alert("Доступ запрещен !")
+//     } 
+    
+
+//Задача 4
+
+// const weight = prompt("Введите вес посылки", 0)
+// const delivery = prompt("Введите тип доставки", 0)
+// let price = 0; 
+// let coefficient = 0;
+
+// if (weight <= 0) {
+//     alert("Некорректный вес посылки")
+// } else if (delivery !== "Стандарт" && delivery !== "Экспресс" && delivery !== "Премиум") {
+//     alert("Неверный тип доставки")
+// } 
+
+// if (delivery < 1 ? price = 5 : delivery <= 5 ? price = 10 : delivery > 5 ? price = 15 : 0 ){
+//     alert(`Базовая стоимость Вашего заказа составляет ${price} рублей`)
+// }
+
+
+
+
+
+
+
+
