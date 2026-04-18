8ba7010ce69982f937347b397cb03b554555bde7
Vitalyadja <m9tacss1337@mail.ru>
2026-04-18 20:29:35 +0300

    Add files via upload

---
+++ JS-urok-24/main.js
@@ -0,0 +1,32 @@
+//Задача 1
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 4) {
+//         console.log(i);
+//     }
+//     continue;
+// }
+
+
+//Задача 2
+
+// const number = +prompt("Введите начальное число:");
+// let factorial = 1;
+
+// for (let i = 1; i < number; i++) {
+//     factorial += i * factorial;
+//     console.log(factorial);
+// } 
+
+//Задача 3
+
+// let board = "";
+
+// for (i = 1; i < 8; i++){
+//     let row = "";
+//     for (j = 1; j < 8; j++){
+//         row += "Ч ";
+//     }
+//     board += row + column;
+// }
+// console.log(board);
